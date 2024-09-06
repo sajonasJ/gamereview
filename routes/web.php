b1fe67df4de2a06a7ed46e95d472bdca692c16a9
sajonasJ <jonas.sajonas@gmail.com>
2024-09-06 10:15:33 +1000

    layout added

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
     return view('home');
-});
-
-Route::get('/manufacturer', function () {
-    return view('manufacturer');
 });
