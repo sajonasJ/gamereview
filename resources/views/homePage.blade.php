698812f9af5b7e99482d4ccbc496c9bbb780c012
sajonasJ <jonas.sajonas@gmail.com>
2024-09-07 10:58:36 +1000

    added new blade formatter

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -11,14 +11,14 @@
 @endsection
 
 @section('content')
-<main class='homePage-content w-100'>
-    <p>home</p>
-    <p>list all item reviews</p>
-    <p>should show all stars per review</p>
-    <p>should show the number of comments per review</p>
-    <p>show names of the items</p>
-    <p>show picture of the item</p>
-</main>
+    <main class='homePage-content w-100'>
+        <p>home</p>
+        <p>list all item reviews</p>
+        <p>should show all stars per review</p>
+        <p>should show the number of comments per review</p>
+        <p>show names of the items</p>
+        <p>show picture of the item</p>
+    </main>
 @endsection
 
 @section('footer')
